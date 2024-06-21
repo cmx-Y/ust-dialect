d5bde0a1ef2eb79db7c018a119c8588c38afdf8b
cmx-Y <chenxingyan@mail.ustc.edu.cn>
2024-06-21 23:38:54 +0800

    [Pass] finish whileBuilder part of LoopBoundConstant pass

--- lib/Transforms/LoopBoundConstant.cpp
+++ lib/Transforms/LoopBoundConstant.cpp
@@ -39,9 +39,6 @@ void LoopBoundConstant(func::FuncOp &func) {
       continue;
     }
     else {
-      OpBuilder ifBuilder(op);
-      //auto ifOp = ifBuilder.create<scf::IfOp>(op->getLoc(), lb , false);
-
       // ::mlir::Region &getBody();
       // Block &front();
       // Operation &front();
@@ -51,6 +48,47 @@ void LoopBoundConstant(func::FuncOp &func) {
       auto ubConstOp = constBuilder.create<arith::ConstantIndexOp>(funcFirstOp.getLoc(), 8);
       // loopOp.setLowerBound(lbConstOp);
       // loopOp.setUpperBound(ubConstOp);
+
+      // Arguments for scf::WhileOp
+      SmallVector<Type> lcvTypes;
+      SmallVector<Location> lcvLocs;
+      lcvTypes.push_back(lb.getType());
+      lcvLocs.push_back(lb.getLoc());
+      // for (Value value : loopOp.getInitArgs()) {
+      //   lcvTypes.push_back(value.getType());
+      //   lcvLocs.push_back(value.getLoc());
+      // }
+
+      SmallVector<Value> initArgs;
+      initArgs.push_back(loopOp.getLowerBound());
+      //llvm::append_range(initArgs, loopOp.getInitArgs());
+
+      OpBuilder whileBuilder(op);
+      auto whileOp = whileBuilder.create<scf::WhileOp>(loopOp.getLoc(), lcvTypes, initArgs, loopOp->getAttrs());
+
+      // 'before' region contains the loop condition and forwarding of iteration
+      // arguments to the 'after' region.
+      auto *beforeBlock = whileBuilder.createBlock(&whileOp.getBefore(), whileOp.getBefore().begin(), lcvTypes, lcvLocs);
+      whileBuilder.setInsertionPointToStart(&whileOp.getBefore().front());
+
+      OpBuilder beforeBuilder = OpBuilder::atBlockBegin(&whileOp.getBefore().front());
+      auto cmpOp = beforeBuilder.create<arith::CmpIOp>(whileOp.getLoc(), arith::CmpIPredicate::slt, lb, ub);
+      whileBuilder.create<scf::ConditionOp>(whileOp.getLoc(), cmpOp.getResult(), beforeBlock->getArguments());
+
+      // 'after' region contains the loop body and the loop increment
+      auto *afterBlock = whileBuilder.createBlock(&whileOp.getAfter(), whileOp.getAfter().begin(), lcvTypes, lcvLocs);
+      whileBuilder.setInsertionPointToEnd(afterBlock);
+      auto ivIncOp = whileBuilder.create<arith::AddIOp>(
+        whileOp.getLoc(), loopOp.getLowerBound(), ubConstOp.getResult());
+      
+      SmallVector<Value> yieldArgs;
+      yieldArgs.push_back(ivIncOp.getResult());
+      auto yieleOp = whileBuilder.create<scf::YieldOp>(whileOp->getLoc(), yieldArgs);
+
+      // Test OpBuilder
+      // OpBuilder builder(op);
+      // auto addiOp = builder.create<arith::AddIOp>(
+      //   loopOp.getLoc(), loopOp.getLowerBound(), ubConstOp.getResult());
     }
   }
 

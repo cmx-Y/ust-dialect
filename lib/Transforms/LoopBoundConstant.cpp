d915193e087caf2d2581c36b361e4d63df368511
cmx-Y <chenxingyan@mail.ustc.edu.cn>
2024-06-22 23:00:40 +0800

    [Pass] ensure same name can occur in while op's two block & split long function to short

--- lib/Transforms/LoopBoundConstant.cpp
+++ lib/Transforms/LoopBoundConstant.cpp
@@ -32,20 +32,20 @@ void LoopBoundConstant(func::FuncOp &func) {
     auto loopOp = cast<scf::ForOp>(op);
     auto lb = loopOp.getLowerBound();
     auto ub = loopOp.getUpperBound();
-    auto step = loopOp.getStep();
     
     // If the lower bound and upper bound are both constants, skip
-    if (lb.getDefiningOp<arith::ConstantOp>() && ub.getDefiningOp<arith::ConstantOp>()) {
+    if (lb.getDefiningOp<arith::ConstantOp>()
+     && ub.getDefiningOp<arith::ConstantOp>()) {
       continue;
     }
     else {
-      // ::mlir::Region &getBody();
-      // Block &front();
-      // Operation &front();
+      // Define two constant operation for lower bound and upper bound
       Operation& funcFirstOp = func.getBody().front().front();
       OpBuilder constBuilder(&funcFirstOp);
-      auto lbConstOp = constBuilder.create<arith::ConstantIndexOp>(funcFirstOp.getLoc(), 0);
-      auto ubConstOp = constBuilder.create<arith::ConstantIndexOp>(funcFirstOp.getLoc(), 8);
+      auto lbConstOp = constBuilder.create<arith::ConstantIndexOp>(
+        funcFirstOp.getLoc(), 0);
+      auto ubConstOp = constBuilder.create<arith::ConstantIndexOp>(
+        funcFirstOp.getLoc(), 8);
       // loopOp.setLowerBound(lbConstOp);
       // loopOp.setUpperBound(ubConstOp);
 
@@ -54,29 +54,27 @@ void LoopBoundConstant(func::FuncOp &func) {
       SmallVector<Location> lcvLocs;
       lcvTypes.push_back(lb.getType());
       lcvLocs.push_back(lb.getLoc());
-      // for (Value value : loopOp.getInitArgs()) {
-      //   lcvTypes.push_back(value.getType());
-      //   lcvLocs.push_back(value.getLoc());
-      // }
-
       SmallVector<Value> initArgs;
       initArgs.push_back(loopOp.getLowerBound());
-      //llvm::append_range(initArgs, loopOp.getInitArgs());
 
-      OpBuilder whileBuilder(op);
-      auto whileOp = whileBuilder.create<scf::WhileOp>(loopOp.getLoc(), lcvTypes, initArgs, loopOp->getAttrs());
+      // Build scf::WhileOp
+      OpBuilder whileBuilder(loopOp);
+      auto whileOp = whileBuilder.create<scf::WhileOp>(loopOp.getLoc(), 
+        lcvTypes, initArgs, loopOp->getAttrs());
 
-      // 'before' region contains the loop condition and forwarding of iteration
-      // arguments to the 'after' region.
-      auto *beforeBlock = whileBuilder.createBlock(&whileOp.getBefore(), whileOp.getBefore().begin(), lcvTypes, lcvLocs);
+      // Build before block
+      auto *beforeBlock = whileBuilder.createBlock(
+        &whileOp.getBefore(), whileOp.getBefore().begin(), lcvTypes, lcvLocs);
       whileBuilder.setInsertionPointToStart(&whileOp.getBefore().front());
 
-      OpBuilder beforeBuilder = OpBuilder::atBlockBegin(&whileOp.getBefore().front());
-      auto cmpOp = beforeBuilder.create<arith::CmpIOp>(whileOp.getLoc(), arith::CmpIPredicate::slt, lb, ub);
-      whileBuilder.create<scf::ConditionOp>(whileOp.getLoc(), cmpOp.getResult(), beforeBlock->getArguments());
+      auto cmpOp = whileBuilder.create<arith::CmpIOp>(
+        whileOp.getLoc(), arith::CmpIPredicate::slt, lb, ub);
+      whileBuilder.create<scf::ConditionOp>(whileOp.getLoc(), cmpOp.getResult(),
+                                            beforeBlock->getArguments());
 
-      // 'after' region contains the loop body and the loop increment
-      auto *afterBlock = whileBuilder.createBlock(&whileOp.getAfter(), whileOp.getAfter().begin(), lcvTypes, lcvLocs);
+      // Build after block
+      auto *afterBlock = whileBuilder.createBlock(
+        &whileOp.getAfter(), whileOp.getAfter().begin(), lcvTypes, lcvLocs);
       whileBuilder.setInsertionPointToEnd(afterBlock);
       auto ivIncOp = whileBuilder.create<arith::AddIOp>(
         whileOp.getLoc(), loopOp.getLowerBound(), ubConstOp.getResult());
@@ -89,6 +87,7 @@ void LoopBoundConstant(func::FuncOp &func) {
       // OpBuilder builder(op);
       // auto addiOp = builder.create<arith::AddIOp>(
       //   loopOp.getLoc(), loopOp.getLowerBound(), ubConstOp.getResult());
+      // addiOp->moveAfter(afterBlock, afterBlock->begin());
     }
   }
 

1264fe73b40ce73b0fbd04188c2734d06430d5b4
cmx-Y <chenxingyan@mail.ustc.edu.cn>
2024-07-25 19:33:13 +0800

    [Dialect] Add USTTypes

---
+++ include/ust/Dialect/USTTypes.h
@@ -0,0 +1,11 @@
+
+#ifndef USTTYPES_H
+#define USTTYPES_H
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "ust/Dialect/USTTypes.h.inc"
+
+#endif // USTTYPES_H

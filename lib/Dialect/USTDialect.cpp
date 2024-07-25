e26a5a10c3432f3b8700cc35fcb7c3bcd0f362bc
cmx-Y <chenxingyan@mail.ustc.edu.cn>
2024-07-25 19:43:35 +0800

    [Dialect] Add SparseInfoAttr

--- lib/Dialect/USTDialect.cpp
+++ lib/Dialect/USTDialect.cpp
@@ -7,6 +7,7 @@
 #include "llvm/ADT/TypeSwitch.h"
 
 #include "ust/Dialect/USTDialect.h"
+#include "ust/Dialect/USTTypes.h"
 
 using namespace mlir;
 using namespace mlir::ust;
@@ -15,6 +16,9 @@ using namespace mlir::ust;
 
 #define GET_ATTRDEF_CLASSES
 #include "ust/Dialect/USTAttrs.cpp.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "ust/Dialect/USTTypes.cpp.inc"
 
 
 //===----------------------------------------------------------------------===//
@@ -41,3 +45,24 @@ void ust::setSparseInfo(Operation *op, int64_t posSize) {
       SparseInfoAttr::get(op->getContext(), posSize);
   setSparseInfo(op, sparseInfo);
 }
+
+Attribute SparseInfoAttr::parse(AsmParser &p, Type type) {
+  StringRef posSizeKw;
+  int64_t posSize;
+  if (p.parseLess() || p.parseKeyword(&posSizeKw) || p.parseEqual() ||
+      p.parseInteger(posSize) || p.parseComma() ||
+      p.parseGreater())
+    return Attribute();
+
+  if (posSizeKw != "posSize")
+    return Attribute();
+
+  return SparseInfoAttr::get(p.getContext(), posSize);
+}
+
+void SparseInfoAttr::print(AsmPrinter &p) const {
+  p << "<posSize=" << getPosSize() << ">";
+}
+
+#define GET_OP_CLASSES
+#include "ust/Dialect/USTOps.cpp.inc"
